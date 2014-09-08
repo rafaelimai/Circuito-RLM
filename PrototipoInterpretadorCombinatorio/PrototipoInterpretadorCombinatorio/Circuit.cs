20b966902f7bc921ed474df7f2d0498139ac6493
rafaelimai <rafael_imai@yahoo.com.br>
2014-09-08 11:41:20 -0300

    Adicionando novas coisas no protótipo auxiliar.

--- PrototipoInterpretadorCombinatorio/PrototipoInterpretadorCombinatorio/Circuit.cs
+++ PrototipoInterpretadorCombinatorio/PrototipoInterpretadorCombinatorio/Circuit.cs
@@ -13,14 +13,105 @@ namespace CombinatoryParserPrototype
      */
     class Circuit
     {
-        private Dictionary<StatePoint, StatePoint> connections;
+        
+        private List<Connection> connections;
         private List<StatePoint> inputPorts;
         private List<StatePoint> outputPorts;
+        private List<LogicGate> logicGates;
 
+        public Circuit()
+        {
+            connections = new List<Connection>();
+            inputPorts = new List<StatePoint>();
+            outputPorts = new List<StatePoint>();
+            logicGates = new List<LogicGate>();
+        }
 
         public void refreshOutputs()
         {
 
         }
+
+        
+
+        //Methods to add or remove input/output ports and logic gates
+        
+        public void addInputPort(StatePoint A)
+        {
+            inputPorts.Add(A);
+        }
+
+        public void addOutputPort(StatePoint A)
+        {
+            outputPorts.Add(A);
+        }
+
+        public void removeInputPort(StatePoint A)
+        {
+            inputPorts.Remove(A);
+        }
+
+        public void removeOutputPort(StatePoint A)
+        {
+            outputPorts.Remove(A);
+        }
+
+        public void addLogicGate(LogicGate gate)
+        {
+            logicGates.Add(gate);
+        }
+
+        public void removeLogicGate(LogicGate gate)
+        {
+            logicGates.Remove(gate);
+        }
+
+        public void connectTwoPoints(StatePoint A, StatePoint B)
+        {
+            Connection c = new Connection(A, B);
+            connections.Add(c);
+        }
+
+        public void disconnectTwoPoints(StatePoint A, StatePoint B)
+        {
+
+            int i = 0;
+            bool mustRun = true;
+
+            while (i < connections.Count && mustRun)
+            {
+                Connection c = connections.ElementAt(i);
+                if (c.getStartPoint().Equals(A) && c.getEndPoint().Equals(B))
+                {
+                    connections.Remove(c);
+                    mustRun = false;
+                } 
+                //Trying to save some procedures.
+            }
+            
+        }
+
+
+        //Getters to retrieve the input/output ports, logic gates and connections. Can be used to graphical purposes.
+        public List<StatePoint> getOutputPorts()
+        {
+            return outputPorts;
+        }
+
+        public List<StatePoint> getInputPorts()
+        {
+            return inputPorts;
+        }
+
+        public List<Connection> getConnections()
+        {
+            return connections;
+        }
+
+        public List<LogicGate> getLogicGates()
+        {
+            return logicGates;
+        }
+
     }
 }
---
+++ PrototipoInterpretadorCombinatorio/PrototipoInterpretadorCombinatorio/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CombinatoryParserPrototype
+{
+    class Connection
+    {
+        /*
+         * The class connection represent a connection between two given points (called here as Start Point and End Point). Connection handling is processed in the class Circuit.
+         */
+
+        private StatePoint startPoint;
+        private StatePoint endPoint;
+
+        public Connection(StatePoint startPoint, StatePoint endPoint)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+        }
+
+        public StatePoint getEndPoint()
+        {
+            return endPoint;
+        }
+
+        public StatePoint getStartPoint()
+        {
+            return startPoint;
+        }
+    }
+}
