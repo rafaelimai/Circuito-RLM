c245a7046346a5f3f799c9033da2f181f8a26fee
rafaelimai <rafael_imai@yahoo.com.br>
2014-09-14 15:33:46 -0300

    Adicionando projeto de internacionalização novo, o arquivo do Manjubator5000 e criando a pasta de Recursos Auxiliares.

---
+++ DialogIntlHandling/DialogIntlHandling/DialogIntlHandling/Dialog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DialogIntlHandling
+{
+
+    /*
+     * The class Dialog abstracts a real-life dialog, that can be translated to various languages.
+     * WARNING: Languages here are represented through a combination of ISO 3166-1 (countries) and ISO 639-1 (languages) standards.
+     * e.g, pt-br (Brazilian Portuguese) is composed by a combination of the above standards. "pt" is the ISO 639-1 code for portuguese, and "br" is the ISO 3166-1 code for Brazil.
+     * This notation is adopted when the language presents derivations for different countries. Using the example above, pt-pt represent the Portuguese Portuguese.
+     * Mostly, languages don't demand the additional country specification.In this case, the country code is disregarded (i.e, only the ISO 639-1 language code is used).
+     * 
+     */
+    public class Dialog
+    {
+
+        /*
+         * List of the translations avaliable for this dialog. The first string represents the language (through the use of ISO 3166-1 and ISO 639-1 standards),
+         *  and the second string the translation itself.
+         */
+        private Dictionary<String, String> translationList;
+
+        //The default language (the original language used to write the dialog), also adopting the above standard.
+        private string defaultLanguage {get;set;}
+
+        //Flag that indicates that this dialog needs to be reviewed.
+        private Boolean needsToBeRevised { get; set; }
+
+        public Dialog()
+        {
+            translationList = new Dictionary<string, string>();
+        }
+
+        public Dialog(string defaultLanguage)
+        {
+            translationList = new Dictionary<string, string>();
+            this.defaultLanguage = defaultLanguage;
+        }
+
+        //Add a new translation to the dialog, given a standardized language and the new translation.
+        public void addTranslation(String language, String translation)
+        {
+            translationList.Add(language, translation);
+        }
+
+        //Retrieves a translation of the dialog, given an input language.
+        //Returns "Translations of this dialog on the given language were not found." if the required translation is not found.
+        public string getTranslation(String language)
+        {
+            String value = "Translations of this dialog on the given language were not found.";
+            translationList.TryGetValue(language, out value);
+            return value;
+        }
+
+        //Check if there is a translation of the dialog in the given language.
+        //Return values indicate the answer.
+        public bool isAvaliableOnLanguage(string language)
+        {
+            String value = null;
+            translationList.TryGetValue(language, out value);
+
+            if (value == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+      
+                
+    }
+}
---
+++ DialogIntlHandling/DialogIntlHandling/DialogIntlHandling/DialogList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DialogIntlHandling
+{
+    class DialogList
+    {
+        private List<Dialog> dialogList;
+        
+        public DialogList()
+        {
+            dialogList = new List<Dialog>();
+        }
+
+        public Dialog retrieveEntry(int entry)
+        {
+
+        }
+
+    }
+}
